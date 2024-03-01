5f3fdcbdb844a196eaaedbe951a6f0402a9e98a8
StefVuck <stefanvuck@gmail.com>
2024-03-01 23:33:42 +0000

    Created electron app to create desktop app

---
+++ main.js
@@ -0,0 +1,64 @@
+const { app, BrowserWindow, screen } = require('electron');
+const { spawn } = require('child_process');
+const http  = require('http');
+const path = require('path');
+
+function createPythonSubprocess() {
+  // Define the path to your Python executable
+  // If Python is in your PATH, you might just use 'python'
+  // Adjust the path to your actual Python script
+  const pythonExecutable = 'python'; // or python3 on some systems
+  const scriptPath = './dash_app/APIpoller.py';
+
+  // Spawn the Python subprocess
+  const pythonProcess = spawn(pythonExecutable, [scriptPath]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  pythonProcess.on('close', (code) => {
+    console.log(`Python subprocess exited with code ${code}`);
+  });
+}
+
+// Call the function to start the Python script
+createPythonSubprocess();
+
+function createWindow() {
+
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: width,
+    height: height,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    icon: path.join(__dirname, './dash_app/assets/icon.png')
+  });
+
+  // and load the Dash app's URL
+  win.loadURL('http://127.0.0.1:8050'); // Use your Dash app's URL
+}
+function checkDashServerReady() {
+    http.get('http://127.0.0.1:8050', (resp) => {
+        if (resp.statusCode === 200) {
+            // If server is ready, create the Electron window
+            setTimeout(createWindow, 1000); 
+        } else {
+            // If server is not ready, check again after a delay
+            console.log('Waiting for Dash server to become ready...');
+            setTimeout(checkDashServerReady, 1000);
+        }
+    }).on("error", (err) => {
+        console.log('Waiting for Dash server to start...');
+        setTimeout(checkDashServerReady, 1000);
+    });
+}
+
+app.whenReady().then(checkDashServerReady).catch(err => console.error('Failed to launch:', err));
